c9948341776febda558cb17b2f83618c696894fd
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-17 19:57:51 -0300

    retornando ao código anterior do radio button

--- script/script.js
+++ script/script.js
@@ -5,39 +5,18 @@ for (let i = 0; i < radio.length; i++){
     radio[i].addEventListener('click', marcar);
 
     function marcar(){
-        radio[i].style.backgroundColor = "#2D2321";
-        radio[i].style.boxShadow = "2px 2px 2px #1003005d";
-        // if(i === 0){
-        //     radio[1].classList.remove('radio-selecionado');
-        //     radio[1].classList.remove('radio-selecionado')
-        //     radio[2].classList.remove('radio-selecionado');
-        //     radio[1].classList.remove('radio-selecionado')
-        // } else if (i === 1){
-        //     radio[0].classList.remove('radio-selecionado');
-        //     radio[2].classList.remove('radio-selecionado');
-        //     radio[0].classList.remove('radio-selecionado');
-        //     radio[2].classList.remove('radio-selecionado');
-        // } else if (i === 2){
-        //     radio[0].classList.remove('radio-selecionado');
-        //     radio[1].classList.remove('radio-selecionado');
-        //     radio[0].classList.remove('radio-selecionado');
-        //     radio[1].classList.remove('radio-selecionado');
-        // }
+        radio[i].classList.add('radio-selecionado');
+        if(i === 0){
+            radio[1].classList.remove('radio-selecionado');
+            radio[2].classList.remove('radio-selecionado');
+        } else if (i === 1){
+            radio[0].classList.remove('radio-selecionado');
+            radio[2].classList.remove('radio-selecionado');
+        } else if (i === 2){
+            radio[0].classList.remove('radio-selecionado');
+            radio[1].classList.remove('radio-selecionado');
+        }
     }
-
-    // function marcar(){
-    //     radio[i].classList.add('radio-selecionado');
-    //     if(i === 0){
-    //         radio[1].classList.remove('radio-selecionado');
-    //         radio[2].classList.remove('radio-selecionado');
-    //     } else if (i === 1){
-    //         radio[0].classList.remove('radio-selecionado');
-    //         radio[2].classList.remove('radio-selecionado');
-    //     } else if (i === 2){
-    //         radio[0].classList.remove('radio-selecionado');
-    //         radio[1].classList.remove('radio-selecionado');
-    //     }
-    // }
 }
 
 function clicarMenuMobile(){
