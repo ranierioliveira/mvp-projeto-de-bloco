36b3e7b9eb056566800ce4a3dd47f7918e5974c7
Ranieri Oliveira <114862419+ranierioliveira@users.noreply.github.com>
2023-05-18 17:06:33 -0300

    Atribuindo funcionalidade ao botão marcar e desmarcar todos

--- script/script.js
+++ script/script.js
@@ -1,27 +1,8 @@
-const radio = document.querySelectorAll('.radiobutton-item-circulo');
 const menu = document.querySelector('.menu');
-const teste = document.querySelector('.quem-somos');
+const radio = document.querySelectorAll('.radiobutton-item-circulo');
+const check = document.querySelectorAll('.checkbox-item-selecao');
+const certo = document.querySelectorAll('.check');
 
-teste.addEventListener('click' , trocarCor);
-
-
-for (let i = 0; i < radio.length; i++){
-    radio[i].addEventListener('click', marcar);
-
-    function marcar(){
-        radio[i].classList.add('radio-selecionado');
-        if(i === 0){
-            radio[1].classList.remove('radio-selecionado');
-            radio[2].classList.remove('radio-selecionado');
-        } else if (i === 1){
-            radio[0].classList.remove('radio-selecionado');
-            radio[2].classList.remove('radio-selecionado');
-        } else if (i === 2){
-            radio[0].classList.remove('radio-selecionado');
-            radio[1].classList.remove('radio-selecionado');
-        }
-    }
-}
 
 function clicarMenuMobile(){
     if(menu.style.display === 'none'){
@@ -39,6 +20,51 @@ function mudouTamanho(){
     }
 }
 
-function trocarCor(){
-    this.classList.toggle('fundo-reserva');
+for (let i = 0; i < radio.length; i++){
+    radio[i].addEventListener('click', marcar);
+
+    function marcar(){
+        this.classList.toggle('radio-selecionado');
+        if(i === 0){
+            radio[1].classList.remove('radio-selecionado');
+            radio[2].classList.remove('radio-selecionado');
+        } else if (i === 1){
+            radio[0].classList.remove('radio-selecionado');
+            radio[2].classList.remove('radio-selecionado');
+        } else if (i === 2){
+            radio[0].classList.remove('radio-selecionado');
+            radio[1].classList.remove('radio-selecionado');
+        }
+    }
+}
+
+for(let i = 0; i < check.length; i++){
+    check[i].addEventListener('click',  marcarCheck);
+
+    function marcarCheck(){
+        if(i === 0){
+            //detecta se os elementos não estão selecionados
+            if(!(check[1].classList.contains('checkbox-selecionado') && check[2].classList.contains('checkbox-selecionado'))){
+                for(let j = 0; j < check.length; j++){
+                    check[j].classList.add('checkbox-selecionado');
+                    certo[j].style.display = "block";
+                }
+            } else if (check[1].classList.contains('checkbox-selecionado') && !(check[2].classList.contains('checkbox-selecionado'))) {
+                for(let j = 0; j < check.length; j++){
+                    check[j].classList.remove('checkbox-selecionado');
+                    certo[j].style.display = "none";
+                }
+            } else if (check[2].classList.contains('checkbox-selecionado') && !(check[1].classList.contains('checkbox-selecionado'))){
+                for(let j = 0; j < check.length; j++){
+                    check[j].classList.remove('checkbox-selecionado');
+                    certo[j].style.display = "none";
+                }
+            } else  {
+                for(let j = 0; j < check.length; j++){
+                    check[j].classList.remove('checkbox-selecionado');
+                    certo[j].style.display = "none";
+                }
+            }
+        }
+    }
 }
